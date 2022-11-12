817781c83968d73f67efb63ca4688eaa7d619d67
spara <sparanofrancesco@hotmail.it>
2022-11-12 22:33:10 +0100

    Graphics

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -378,19 +378,19 @@ class BarcodeScannerViewController: UIViewController {
         cancelButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
         cancelButton.widthAnchor.constraint(equalToConstant: 40.0).isActive = true
         */
-
+        /*
         cancelButton.translatesAutoresizingMaskIntoConstraints = true
         cancelButton.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
         cancelButton.heightAnchor.constraint(equalToConstant: 70.0).isActive = true
         cancelButton.bottomAnchor.constraint(equalTo:view.bottomAnchor,constant: 0).isActive=true
         //cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:10).isActive = true
-        
+
         switchCameraButton.translatesAutoresizingMaskIntoConstraints = true
         // A little bit to the right.
         switchCameraButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
         switchCameraButton.heightAnchor.constraint(equalToConstant: 70.0).isActive = true
         switchCameraButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-
+        */
     }
     
     /// Flash button click event listener
