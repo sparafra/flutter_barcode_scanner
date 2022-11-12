1e049b716bb4e55d82ec65144c7818a9b527933c
spara <sparanofrancesco@hotmail.it>
2022-11-12 22:39:55 +0100

    Graphics

--- ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
+++ ios/Classes/SwiftFlutterBarcodeScannerPlugin.swift
@@ -374,9 +374,9 @@ class BarcodeScannerViewController: UIViewController {
         flashIcon.widthAnchor.constraint(equalToConstant: 40.0).isActive = true
 
         cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
-        cancelButton.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
-        cancelButton.widthAnchor.constraint(equalToConstant: 40.0).isActive = true
+        cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 70.0).isActive = true
+        cancelButton.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
 
         /*
         cancelButton.translatesAutoresizingMaskIntoConstraints = true
